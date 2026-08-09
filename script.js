961c33cfd1d4b2b32b75ea76f34b23092c681078
Ravikiran <ravikiranravella6@gmail.com>
2026-08-09 13:15:46 +0530

    Update Google Sheets URL to new deployment

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const year = document.querySelector("[data-year]");
 
 // Replace this with your deployed Google Apps Script web app URL
 //const SHEETS_URL = 'https://script.google.com/macros/s/AKfycbwO0qJDse5zy5gkaH0_SwOoVaoNpR_VQKjiaA1r3mymzyTOha2UrClXK3ruKNNEN5dxsA/exec';
-const SHEETS_URL = "https://script.google.com/macros/s/AKfycbwCItnpg0nAAx6E4MDGK_8YIJA6E3r60WSmYZzcOxTDBmqGLpi6wkYybvT4aKM45ghPgg/exec";
+const SHEETS_URL = "https://script.google.com/macros/s/AKfycbwjGJVd-2Gv1tx-XUXUHBJVW_ACY0Wgjh_PBJ69J2aM3s82fwpG7fyW3L-tyBs_aLYOfA/exec";
 //const SHEETS_URL = "https://script.google.com/macros/s/AKfycbyn13ybIVbmmfiX6iNjlsJ37FJxLzPaWGqEAD9yWYSEaFQMcK_bT3xGcKAd2xerILnTYw/exec";
 
 function updateHeader() {
